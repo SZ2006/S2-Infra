4c01c539d0b25824ba7e240ccf63e23ea7aef2d2
Salaheddine Zaroili <s.zaroili@student.fontys.nl>
2025-02-13 13:42:32 +0100

    GateKeeperV2

--- GateKeeperV1.go
+++ GateKeeperV1.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	fmt.Println("Welkom bij Fonteyn Vakantieparken")
+	uur := time.Now().Hour()
+	if uur >= 7 && uur < 12 {
+		fmt.Println("Goedemorgen! Welkom bij Fonteyn Vakantieparken")
+	} else if uur >= 12 && uur < 18 {
+		fmt.Println("Goedemiddag! Welkom bij Fonteyn Vakantieparken")
+	} else if uur >= 18 && uur < 23 {
+		fmt.Println("Goedenavond! Welkom bij Fonteyn Vakantieparken")
+	} else {
+		fmt.Println("Sorry, de parkeerplaats is ’s nachts gesloten")
+	}
 }
